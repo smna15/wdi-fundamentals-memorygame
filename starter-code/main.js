6b522dffd14256b98e1ca6ef28f923fb3d6e1af5
smna15 <xsangmin@gmail.com>
2016-09-12 16:04:24 -0400

    added conditions and alerts

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,6 +1,16 @@
-console.log("JS file is connected to HTML! Woo!")
+// console.log("JS file is connected to HTML! Woo!")
 
 var cardOne = "queen"
 var cardTwo = "queen"
 var cardThree = "king"
 var cardFour = "king"
+
+if (cardOne === cardTwo) {
+  // for queen card match
+  alert("You found a match!");
+} else if (cardThree === cardFour) {
+  // for king card match
+  alert("You found a match!");
+} else {
+  alert("Sorry, try again");
+}
