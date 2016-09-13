2c2dc84c8cb8e34982461c3fceb3b952973dacc3
smna15 <xsangmin@gmail.com>
2016-09-13 13:36:17 -0400

    create board with javascript

--- starter-code/main.js
+++ starter-code/main.js
@@ -5,12 +5,21 @@ var cardTwo = "queen"
 var cardThree = "king"
 var cardFour = "king"
 
-if (cardOne === cardTwo) {
-  // for queen card match
-  alert("You found a match!");
-} else if (cardThree === cardFour) {
-  // for king card match
-  alert("You found a match!");
-} else {
-  alert("Sorry, try again");
+// if (cardOne === cardTwo) {
+//   // for queen card match
+//   alert("You found a match!");
+// } else if (cardThree === cardFour) {
+//   // for king card match
+//   alert("You found a match!");
+// } else {
+//   alert("Sorry, try again");
+// }
+
+var board = document.getElementById('game-board');
+var card = document.createElement('div').className('card');
+var createBoard = function() {
+  for (var i=0; i < 4; i++) {
+    board.appendChild(card);
 }
+
+createBoard();
