980c62bf1f26f69d51990aecb78ecc1055874609
smna15 <xsangmin@gmail.com>
2016-09-13 15:10:34 -0400

    add card game logic in js

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,25 +1,62 @@
 // console.log("JS file is connected to HTML! Woo!")
 
-var cardOne = "queen"
-var cardTwo = "queen"
-var cardThree = "king"
-var cardFour = "king"
-
-// if (cardOne === cardTwo) {
-//   // for queen card match
-//   alert("You found a match!");
-// } else if (cardThree === cardFour) {
-//   // for king card match
-//   alert("You found a match!");
-// } else {
-//   alert("Sorry, try again");
-// }
+// var cardOne = "queen"
+// var cardTwo = "queen"
+// var cardThree = "king"
+// var cardFour = "king"
 
 var board = document.getElementById('game-board');
-var card = document.createElement('div').className('card');
-var createBoard = function() {
-  for (var i=0; i < 4; i++) {
+
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+
+// create html element for each card with card class
+// append to board
+function createBoard() {
+  for (var i=0; i < cards.length; i++) {
+    var card = document.createElement('div')
+    card.className = "card";
+    card.setAttribute('data-card', cards[i]);
+
     board.appendChild(card);
+  }
+
+  var cardElement = document.querySelector("card");
+  cardElement.addEventListener('click', isTwoCards);
 }
 
+function isMatch() {
+  if (cardOne === cardTwo) {
+    // for queen card match
+    alert("You found a match!");
+  } else if (cardThree === cardFour) {
+    // for king card match
+    alert("You found a match!");
+  } else {
+    alert("Sorry, try again");
+  }
+}
+
+function isTwoCards() {
+  // add card to array of cards in play
+  cardsInPlay.push(this.getAttribute('data-card'));
+  
+  if (this.getAttribute == "queen") {
+    this.innerHTML = "<img src='queen.png' height='200px' width='150px'>";
+  }
+  if (this.getAttribute == "king") {
+    this.innerHTML = "<img src='king.png' height='200px' width='150px'>";
+  }
+
+
+  // if you hae two cards in play check for a match
+  if (cardsInPlay.length === 2) {
+    // pass the cardsInPlay as an argument to isMatch function
+    isMatch(cardsInPlay);
+    // clear cards in play array for next try
+    cardsInPlay = [];
+  }
+}
+
+// create game board with 4 cards
 createBoard();
